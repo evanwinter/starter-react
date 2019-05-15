b3adca188964ba6be1a9b7bf7416f837b8afded5
Evan Winter <ewinter@sccadv.com>
2019-05-15 11:00:27 -0500

    initial commit 🌱

---
+++ App.js
@@ -0,0 +1,33 @@
+import React from "react"
+
+const App = () => {
+	return (
+		<main>
+			<section className="bg-light">
+				<div className="container">
+					<h1>Lorem ipsum</h1>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
+						ut labore et dolore magna aliqua. <a href="https://example.com/" target="_blank">Ut enim ad minim veniam, quis nostrud exercitation ullamco
+						laboris nisi ut aliquip ex ea commodo consequat.</a> Duis aute irure dolor in reprehenderit in
+						voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+						cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					</p>
+				</div>
+			</section>
+			<section className="bg-dark">
+				<div className="container">
+					<h1>Lorem ipsum</h1>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
+						ut labore et dolore magna aliqua. <a href="https://example.com/" target="_blank">Ut enim ad minim veniam, quis nostrud exercitation ullamco
+						laboris nisi ut aliquip ex ea commodo consequat.</a> Duis aute irure dolor in reprehenderit in
+						voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+						cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					</p>
+				</div>
+			</section>
+		</main>
+	)
+}
+export default App
