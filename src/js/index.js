f10070e83d1d475019957626a84bc95569e83bc3
Evan Winter <ewinter@sccadv.com>
2019-06-18 14:32:32 -0500

    move scss import to html file

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import "babel-polyfill"
-import "../scss/index.scss"
 import App from "./App"
 
 ReactDOM.render(<App />, document.querySelector("#root"))
