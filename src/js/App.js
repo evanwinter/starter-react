55c610afd741b9e6e9680a5cfe76b4d5ee617517
Evan Winter <ewinter@sccadv.com>
2019-06-19 11:14:18 -0500

    .

--- src/js/App.js
+++ src/js/App.js
@@ -1,87 +1,106 @@
-import React from "react"
+import React, { useState, useRef, useEffect } from "react"
 
 const App = () => {
+
+	const formRef = useRef()
+	const firstNameRef = useRef()
+	const lastNameRef = useRef()
+	const listRef = useRef()
+
+	const [contacts, setContacts] = useState(undefined)
+
+	let db
+
+	const handleSubmit = e => {
+		e.preventDefault()
+		let newItem = { firstName: firstNameRef.current.value, lastName: lastNameRef.current.value }
+		let transaction = db.transaction(['contacts'], 'readwrite')
+		let objectStore = transaction.objectStore('contacts')
+		let request = objectStore.add(newItem)
+
+		request.onsuccess = () => {
+			firstNameRef.current.value = ''
+			lastNameRef.current.value = ''
+		}
+
+		transaction.oncomplete = () => {
+			console.log("Transaction complete")
+		}
+		transaction.onerror = () => {
+			console.log("Error on transaction")
+		}
+
+		displayData()
+	}
+
+	// Set up DB
+	useEffect(() => {
+		let request = window.indexedDB.open("contacts", 1)
+
+		request.onerror = () => console.log('Database failed to open')
+
+		request.onsuccess = () => {
+			console.log('Database opened successfully')
+			db = request.result
+		}
+
+		request.onupgradeneeded = (e) => {
+			let db = e.target.result
+
+			let objectStore = db.createObjectStore('contacts', { keyPath: 'id', autoIncrement: true})
+
+			objectStore.createIndex('firstName', 'firstName', { unique: false })
+			objectStore.createIndex('lastName', 'lastName', { unique: false })
+
+			console.log("Database setup complete")
+		}
+
+	}, [])
+
+	const displayData = () => {
+		listRef.current.innerHTML = ''
+
+		const objectStore = db.transaction('contacts').objectStore('contacts')
+		const nextContacts = []
+		objectStore.openCursor().onsuccess = e => {
+			let cursor = e.target.result
+			if (cursor) {
+				nextContacts.push(cursor.value)
+				cursor.continue()
+			}
+			setContacts(nextContacts)
+		}
+	}
+
+	useEffect(() => {
+		displayData()
+	}, [contacts])
+
 	return (
-		<main>
-			<section className="bg-light">
-				<div className="container">
-					<h1>Lorem ipsum</h1>
-					<p>
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
-						ut labore et dolore magna aliqua. <a href="https://example.com/" target="_blank">Ut enim ad minim veniam, quis nostrud exercitation ullamco
-						laboris nisi ut aliquip ex ea commodo consequat.</a> Duis aute irure dolor in reprehenderit in
-						voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-						cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-					</p>
-					<div className="grid two-col">
-						<p>
-							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
-							ut labore et dolore magna aliqua. <a href="https://example.com/" target="_blank">Ut enim ad minim veniam, quis nostrud exercitation ullamco
-							laboris nisi ut aliquip ex ea commodo consequat.</a> Duis aute irure dolor in reprehenderit in
-							voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-							cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-						</p>
-						<p>
-							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
-							ut labore et dolore magna aliqua. <a href="https://example.com/" target="_blank">Ut enim ad minim veniam, quis nostrud exercitation ullamco
-							laboris nisi ut aliquip ex ea commodo consequat.</a> Duis aute irure dolor in reprehenderit in
-							voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-							cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-						</p>
-					</div>
+		<main className="container">
+
+			<form onSubmit={handleSubmit} ref={formRef}>
+				<div>
+					<label htmlFor="firstName">First</label>
+					<input ref={firstNameRef} type="text" id="firstName" />
 				</div>
-			</section>
-			<section className="bg-dark">
-				<div className="container">
-					<h1>Lorem ipsum</h1>
-					<div className="grid two-col">
-						<p>
-							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
-							ut labore et dolore magna aliqua. <a href="https://example.com/" target="_blank">Ut enim ad minim veniam, quis nostrud exercitation ullamco
-							laboris nisi ut aliquip ex ea commodo consequat.</a> Duis aute irure dolor in reprehenderit in
-							voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-							cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-						</p>
-						<p>
-							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
-							ut labore et dolore magna aliqua. <a href="https://example.com/" target="_blank">Ut enim ad minim veniam, quis nostrud exercitation ullamco
-							laboris nisi ut aliquip ex ea commodo consequat.</a> Duis aute irure dolor in reprehenderit in
-							voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-							cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-						</p>
-					</div>
-					<div className="grid four-col">
-						<p>
-							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
-							ut labore et dolore magna aliqua. <a href="https://example.com/" target="_blank">Ut enim ad minim veniam, quis nostrud exercitation ullamco
-							laboris nisi ut aliquip ex ea commodo consequat.</a> Duis aute irure dolor in reprehenderit in
-							voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-							cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-						</p>
-						<p>
-							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
-							ut labore et dolore magna aliqua. <a href="https://example.com/" target="_blank">Ut enim ad minim veniam, quis nostrud exercitation ullamco
-							laboris nisi ut aliquip ex ea commodo consequat.</a> Duis aute irure dolor in reprehenderit in
-							voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-							cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-						</p>
-						<p>
-							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
-							ut labore et dolore magna aliqua. <a href="https://example.com/" target="_blank">Ut enim ad minim veniam, quis nostrud exercitation ullamco
-							laboris nisi ut aliquip ex ea commodo consequat.</a> Duis aute irure dolor in reprehenderit in
-							voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-							cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-						</p>
-						<p>
-							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
-							ut labore et dolore magna aliqua. <a href="https://example.com/" target="_blank">Ut enim ad minim veniam, quis nostrud exercitation ullamco
-							laboris nisi ut aliquip ex ea commodo consequat.</a> Duis aute irure dolor in reprehenderit in
-							voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-							cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-						</p>
-					</div>
+				<div>
+					<label htmlFor="lastName">Last</label>
+					<input ref={lastNameRef} type="text" id="lastName" />
 				</div>
-			</section>
+				<button type="submit">Submit</button>
+			</form>
+
+			<div>
+				<ul ref={listRef}>
+					{contacts && contacts.map((contact, index) => {
+						return <li key={index}>{contact.firstName} {contact.lastName}</li>
+					})}
+				</ul>
+			</div>
+
+
+
 		</main>
 	)
 }
