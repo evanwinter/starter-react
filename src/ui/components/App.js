5b916d9ed0dcce415551e37631eaf25ca255db35
Evan Winter <evan.winter@brookfieldpropertiesretail.com>
2021-09-16 09:59:50 -0500

    Update folder structure and starter content

---
+++ src/ui/components/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+export const App = () => {
+  return (
+    <div class="container">
+      <h1>Hello World!</h1>
+      <p>
+        Quis aute id officia culpa velit culpa nostrud esse. Aute commodo et
+        quis pariatur mollit. Nostrud nulla ipsum tempor esse sint. Adipisicing
+        et labore nisi duis duis officia.
+      </p>
+    </div>
+  )
+}
